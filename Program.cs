309a01aee452249c6375eadc80edfed480d7e19a
Matthew Elder <Matthew.elder@hotmail.com>
2023-06-06 15:07:52 -0400

    Final Commit

--- Program.cs
+++ Program.cs
@@ -10,63 +10,54 @@
 
             while (isRunning)
             {
-
+                
 
                 Console.Clear();
-                Console.WriteLine("Welcome to the Character Creator!");
-                Console.WriteLine("Please select which part of the Character Creator you would like to work on:");
+                Console.WriteLine("Welcome to your new rabbit!");
+                Console.WriteLine("Please select how to interact with your rabbit:");
                 Console.WriteLine("1: Name");
                 Console.WriteLine("2: Color");
                 Console.WriteLine("3: Feed your rabbit");
                 Console.WriteLine("4: Let your rabbit sleep");
                 Console.WriteLine("5: Have your rabbit move around");
-                Console.WriteLine("6: Display rabbit statistics");
+                Console.WriteLine("6: Rabbit statistics");
                 Console.WriteLine("If you are finished, simply enter quit");
+                Console.WriteLine("                          .\".\r\n                         /  |\r\n                        /  /\r\n                       / ,\"\r\n           .-------.--- /\r\n          \"._ __.-/ o. o\\  \r\n             \"   (    Y  )\r\n                  )     /\r\n                 /     (\r\n                /       Y\r\n            .-\"         |\r\n           /  _     \\    \\ \r\n          /    `. \". ) /' )\r\n         Y       )( / /(,/\r\n        ,|      /     )\r\n       ( |     /     /\r\n        \" \\_  (__   (__\r\n            \"-._,)--._,)");
                 string userInput = Console.ReadLine().ToLower();
 
                 switch (userInput)
                 {
                     case "1":
+                        Console.Clear();
                         Console.WriteLine("Enter your rabbit's name.");
-                        rabbit.Name = Console.ReadLine();
+                        rabbit.Rename(Console.ReadLine());
                         break;
                     case "2":
+                        Console.Clear();
                         Console.WriteLine("Enter your rabbit's color.");
-                        rabbit.Color = Console.ReadLine();
+                        rabbit.NewColor(Console.ReadLine());
                         break;
                     case "3":
-                        Console.WriteLine("You feed your rabbit a carrot.");
-                        rabbit.Hunger += 50;
-                        if (rabbit.Hunger > 100)
-                        {
-                         rabbit.Hunger = 100;
-                        }
+                        Console.Clear();
+                        rabbit.Eat();
                         break;
                     case "4":
-                        int hoursSlept = sleep.Next(1, 5);
-                        Console.WriteLine($"Your rabbit falls asleep for {hoursSlept} hours.");
-                        rabbit.Hunger = rabbit.Hunger - (hoursSlept * 15);
-                        rabbit.Tiredness = rabbit.Tiredness + hoursSlept * 20;
-                        if (rabbit.Tiredness > 100) { rabbit.Tiredness = 100; }
-                        if (rabbit.Hunger < 0) { rabbit.Hunger = 0; }
-                        Console.WriteLine("Press enter to return.");
-                        Console.ReadKey();
+                        Console.Clear();
+                        rabbit.Sleep(rabbit.Hunger, rabbit.Tiredness);
                         break;
                     case "5":
-                        Console.WriteLine("Let your rabbit move around");
-                        if (rabbit.Hunger == 0) { Console.WriteLine("Your rabbit needs to eat!"); break; }
-
-                        if (rabbit.Hunger < 0)
-                        {
-                            rabbit.Hunger = 0;
-                        }
-                    break;
+                        Console.Clear();
+                        rabbit.Move();
+                        break;
                     case "6":
+                        Console.Clear();
                         Console.WriteLine($"{rabbit.Name} the {rabbit.Color} rabbit");
                         Console.WriteLine("-------------------");
                         Console.WriteLine($"Hunger: {rabbit.Hunger}");
                         Console.WriteLine($"Sleep: {rabbit.Tiredness}");
                         Console.WriteLine("Press any key to continue");
+                        Console.WriteLine("                             ,\r\n                            /|      __\r\n                           / |   ,-~ /\r\n                          Y :|  //  /\r\n                          | jj /( .^\r\n                          >-\"~\"-v\"\r\n                         /       Y\r\n                        jo  o    |\r\n                       ( ~T~     j\r\n                        >._-' _./\r\n                       /   \"~\"  |\r\n                      Y     _,  |\r\n                     /| ;-\"~ _  l\r\n                    / l/ ,-\"~    \\\r\n                    \\//\\/      .- \\\r\n                     Y        /    Y    \r\n                     l       I     !\r\n                     ]\\      _\\    /\"\\\r\n                    (\" ~----( ~   Y.  )\r\n                ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
                         Console.ReadKey();
                         break;
                     case "quit":
@@ -77,21 +68,5 @@
                 }
             }
         }
-
-        //Let your rabbit sleep. Increases sleep and reduces hunger properties
-        public int Sleep(int hunger, int tired)
-        {
-            Random sleep = new Random();
-            int hoursSlept = sleep.Next(1, 5);
-
-            Console.WriteLine($"Your rabbit falls asleep for {hoursSlept} hours.");
-            hunger = hunger - (hoursSlept * 15);
-            rabbit.Tiredness = rabbit.Tiredness + hoursSlept * 20;
-            if (rabbit.Tiredness > 100) { rabbit.Tiredness = 100; }
-            if (rabbit.Hunger < 0) { rabbit.Hunger = 0; }
-
-            return hoursSlept;
-        }
-        
     }
 }
--- Rabbit.cs
+++ Rabbit.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassOfYourChoice
+﻿namespace ClassOfYourChoice
 {
     public class Rabbit
     {
@@ -14,13 +8,15 @@ namespace ClassOfYourChoice
         private int hunger;
         private int tiredness;
         private string movement;
+        private int feet = 0;
 
         //Properties
         public string Name { get; set; }
         public string Color { get; set; }
         public int Hunger { get; set; }
         public int Tiredness { get; set; }
-        public string Movement { get; set; } 
+        public string Movement { get; set; }
+        public int Feet { get; set; }
 
         //Construct
         public Rabbit(string name, string color, int hunger, int tiredness, string movement)
@@ -30,6 +26,97 @@ namespace ClassOfYourChoice
             Hunger = hunger;
             Tiredness = tiredness;
             Movement = movement;
+            Feet = feet;
+        }
+
+        //Methods
+
+        //Rename your rabbit
+        public void Rename(string newName)
+        {
+            Name = newName;
+            Console.WriteLine("You rename your rabbit to " + Name);
+            Console.WriteLine("Press any key to return to the menu");
+            Console.ReadKey();
+        }
+
+        //Rabbit color
+        public void NewColor(string newColor)
+        {
+            Color = newColor;
+            Console.WriteLine("Your rabbit's color is now " + Color);
+            Console.WriteLine("Press any key to return to the menu");
+            Console.ReadKey();
+        }
+        //Rabbit eats
+        //Hunger value increases by 50
+        //Tiredness value decreases by 20
+        public void Eat()
+        {
+            Hunger += 50;
+            if (Hunger > 100) { Hunger = 100; }
+            Tiredness -= 20;
+            if (Tiredness < 0) 
+            { 
+                Tiredness = 0; 
+                Console.WriteLine("Your rabbit needs to sleep first!"); 
+            }
+            else
+            {
+                Console.WriteLine("You feed your rabbit a carrot.");
+            }
+            Console.WriteLine("Press any key to return to the menu");
+            Console.WriteLine("                 \\     /\r\n                 \\\\   //\r\n                  )\\-/(\r\n                  /e e\\\r\n                 ( =T= )\r\n                 /`---'\\\r\n            ____/ /___\\ \\\r\n       \\   /   '''     ```~~\"--.,_\r\n    `-._\\ /                       `~~\"--.,_\r\n   ------>|                                `~~\"--.,_\r\n    _.-'/ \\                            ___,,,---\"\"~~``'\r\n       /   \\____,,,,....----\"\"\"\"~~~~````");
+            Console.ReadKey();
+        }
+
+        //Rabbit sleeps
+        //Hunger value decreases by 15 * 1 hour
+        //Tiredness value increases by 20 * 1 hour
+        public void Sleep(int hungerValue, int tiredValue)
+        {
+            Random random = new Random();
+            int hoursSlept = random.Next(1, 5);
+
+            Hunger -= (hoursSlept * 15);
+            Tiredness += (hoursSlept * 20);
+            if (Tiredness > 100) { Tiredness = 100; }
+            if (Hunger < 0) { Hunger = 0; }
+            Console.WriteLine("Your rabbit slept for " + hoursSlept + " hours.");
+            Console.WriteLine("Press any key to return to the menu");
+            Console.WriteLine("       __\r\n      / \\`\\          __\r\n      |  \\ `\\      /`/ \\\r\n      \\_/`\\  \\-\"-/` /\\  \\\r\n           |       |  \\  |\r\n           (_     _)   \\_/\r\n           /       \\\r\n       ,\".|.'.\\_/.'.|.\",\r\n      /   /\\' _|_ '/\\   \\\r\n      |  /  '-`\"`-'  \\  |\r\n      | |             | |\r\n      | \\    \\   /    / |\r\n       \\ \\    \\ /    / /\r\n        `\"`\\   :   /'\"`\r\n            `\"\"`\"\"`");
+            Console.ReadKey();
+        }
+
+        /*Rabbit moves
+         * Hunger value decreases by 15
+         * Tiredness value decreases by 15
+         * Rabbit total movement counter increases by 5ft * number of hops*/
+        public void Move()
+        {
+            Random random = new Random();
+            int thisMove = random.Next(1, 5);
+            
+            Feet += (thisMove * 5);
+            Hunger -= 15;
+            if (Hunger <= 0) 
+            { 
+                Console.WriteLine("Your rabbit is too hungry to hop around! Feed them first!");
+                Hunger = 0;
+            }
+            Tiredness -= 15;
+            if (Tiredness <= 0)
+            {
+                Console.WriteLine("Your rabbit is too tired to hop around! Let them sleep first!");
+                Tiredness = 0;
+            }
+
+            Console.WriteLine("Your rabbit hops " + thisMove + " times, totalling " + (thisMove * 5) + "ft");
+            Console.WriteLine("Your rabbit has moved a total of " + Feet + "'");
+
+            Console.WriteLine("Press any key to return to the menu");
+            Console.WriteLine("                     _       _\r\n                    / \\     / \\\r\n                   {   }   {   }\r\n                   {   {   }   }\r\n                    \\   \\ /   /\r\n                     \\   Y   /\r\n                     .-\"`\"`\"-.\r\n                   ,`         `.\r\n                  /             \\\r\n                 /               \\\r\n                {     ;\"\";,       }\r\n                {  /\";`'`,;       }\r\n                 \\{  ;`,'`;.     /\r\n                  {  }`\"\"`  }   /}\r\n                  {  }      {  //  \r\n                  {||}      {  /\r\n                  `\"'       `\"'");
+            Console.ReadKey();
         }
     }
 }
